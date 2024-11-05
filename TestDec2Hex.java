9f34fe2b77d31f74e88c454d6ef646624738bce9
ChristieSeery <cseery300@caledonian.ac.uk>
2024-11-05 22:30:12 +0000

    fix unit testing-1

--- TestDec2Hex.java
+++ TestDec2Hex.java
@@ -5,16 +5,23 @@ import static org.junit.Assert.assertEquals;
 public class TestDec2Hex{
     public TestDec2Hex() {}
 
+
     @Test
     public void testInteger(){
-        assertEquals("Expected output F", "F", Dec2Hex.convertToHex(new String[]{"15"}));
+        assertEquals("Number conversion should  work", "F", Dec2Hex.convertToHex(new String[]{"15"}));
 
     }
     @Test
-    public void testIsEmpty(){
-        assertEquals("Cannot be empty", "", Dec2Hex.convertToHex(new String[]{}));
+    public void testNull(){
+        assertEquals("Input was null, this must be a number", "", Dec2Hex.convertToHex(new String[]{null}));
+    }
+
+    @Test
+    public void testString(){
+        assertEquals("Not a number", Dec2Hex.convertToHex(new String[]{"abc"}));
     }
 
 
 }
+
 
