17c45080d993c7610c3576cee614072230df1877
ChristieSeery <cseery300@caledonian.ac.uk>
2024-11-06 13:24:47 +0000

    Suitable Comments

--- TestDec2Hex.java
+++ TestDec2Hex.java
@@ -5,23 +5,28 @@ import static org.junit.Assert.assertEquals;
 public class TestDec2Hex{
     public TestDec2Hex() {}
 
+    // create Dec2Hex object
     Dec2Hex dec2Hex;
 
     @Before
     public void setUp(){
+
+        // new instance of Dec2Hex object
         dec2Hex = new Dec2Hex();
     }
-
+    // Test if the value is an integer
     @Test
     public void testInteger(){
         assertEquals("Number conversion should  work", "F", dec2Hex.convertToHex("15"));
 
     }
+
+    //Test if the value is null
     @Test
     public void testNull(){
         assertEquals("Input was null, enter a number", "", dec2Hex.convertToHex(null));
     }
-
+    //Test if the value is a String
     @Test
     public void testString(){
         assertEquals("Please enter an Integer", dec2Hex.convertToHex("abc"));
@@ -29,5 +34,4 @@ public class TestDec2Hex{
 
 
 }
-
 
--- TestRunner.java
+++ TestRunner.java
@@ -6,26 +6,31 @@ public class TestRunner {
 
 
     public static void main(String[] args) {
+        // instance of result object
         Result result = JUnitCore.runClasses(TestDec2Hex.class);
         int fails = 0;
-
+        // try to get failures
         try{
             for(Failure failure: result.getFailures()){
+                //print failure
                 System.out.println(failure.toString());
                 fails++;
 
             }
+
+		// if fails is greater than zero throw new exception
             if(fails>0)
                 throw new Exception();
             System.out.println(result.wasSuccessful());
-        } catch (Exception e) {
-            System.out.println("" + fails + " tess failed");
+        } catch (Exception e) { //if there is fails print failures
+            System.out.println("" + fails + " test failed");
             System.exit(1);
         }
-
-        System.out.println("Test run count: " + result.getRunCount());
-        System.out.println("Test failed count: " + result.getFailureCount());
-        System.out.println("Test successful: " + result.wasSuccessful());
+        // print test information
+        
+            System.out.println("Test run count: " + result.getRunCount());
+            System.out.println("Test failed count: " + result.getFailureCount());
+            System.out.println("Test successful: " + result.wasSuccessful());
 
 
     }
