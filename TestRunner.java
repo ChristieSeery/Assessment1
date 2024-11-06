07f5ac740feb5184638e7fe19d9b7faed8e38245
ChristieSeery <cseery300@caledonian.ac.uk>
2024-11-06 13:02:53 +0000

    Updated Test Runner

--- TestRunner.java
+++ TestRunner.java
@@ -2,25 +2,31 @@ import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
 
-// took some code from https://toolsqa.com/java/junit-framework/junit-test-runner/
-
-
 public class TestRunner {
+
 
     public static void main(String[] args) {
         Result result = JUnitCore.runClasses(TestDec2Hex.class);
+        int fails = 0;
 
+        try{
+            for(Failure failure: result.getFailures()){
+                System.out.println(failure.toString());
+                fails++;
+
+            }
+            if(fails>0)
+                throw new Exception();
+            System.out.println(result.wasSuccessful());
+        } catch (Exception e) {
+            System.out.println("" + fails + " tess failed");
+            System.exit(1);
+        }
 
         System.out.println("Test run count: " + result.getRunCount());
         System.out.println("Test failed count: " + result.getFailureCount());
         System.out.println("Test successful: " + result.wasSuccessful());
 
-        // Print details of each failure, if any
-        if (!result.wasSuccessful()) {
-            System.out.println("Failed test cases:");
-            for (Failure failure : result.getFailures()) {
-                System.out.println(failure.toString());
-            }
-        }
+
     }
 }
